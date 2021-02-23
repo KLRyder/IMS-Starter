7864623a14438a640482c1fa417b82c51006d6a4
kieran ryder <kieranlryder@gmail.com>
2021-02-23 16:26:21 +0000

    QAP-64 #comment implemented create() in the DAO

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -2,12 +2,19 @@ package com.qa.ims.persistence.dao;
 
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class OrderDAO implements Dao<Order>{
+public class OrderDAO implements Dao<Order> {
+    public static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     public List<Order> readAll() {
         return null;
@@ -20,6 +27,39 @@ public class OrderDAO implements Dao<Order>{
 
     @Override
     public Order create(Order order) {
+        try {
+            Connection connection = DBUtils.getInstance().getConnection();
+
+            connection.setAutoCommit(false);
+
+            // get index of the next order to insert. needed for orderlink creation se we cant rely on the
+            // tables auto increment
+            ResultSet rs = connection.createStatement().executeQuery("SELECT MAX(idorder) FROM `order`");
+            int index = rs.getInt("MAX(idorder)") + 1;
+
+            //create order in order table
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO `order`(idorder, custid) VALUES (?,?)");
+            statement.setLong(1, index);
+            statement.setLong(2, order.getCustomer().getId());
+
+            //create orderLink in order_link table for each attached item(if any)
+            for (Item item : order.getItems().keySet()) {
+                statement = connection.prepareStatement(
+                        "INSERT INTO `order_link`(orderid,itemid,quantity) VALUES (?,?,?)");
+                statement.setLong(1, index);
+                statement.setLong(2, item.getId());
+                statement.setInt(3, order.getItems().get(item));
+            }
+
+            connection.commit();
+            connection.close();
+            return readLatest();
+
+        } catch (SQLException throwables) {
+            LOGGER.debug(throwables);
+            LOGGER.error(throwables.getMessage());
+        }
         return null;
     }
 
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -20,18 +20,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`item`
     PRIMARY KEY (`iditem`)
 );
 
-CREATE TABLE `ims`.`order`
+CREATE TABLE IF NOT EXISTS `ims`.`order`
 (
-    `idorder` INT NOT NULL AUTO_INCREMENT,
-    `custid`  INT NOT NULL,
-    PRIMARY KEY (`idorder`)
+    `idorder` int NOT NULL AUTO_INCREMENT,
+    `custid`  int NOT NULL,
+    PRIMARY KEY (`idorder`),
+    KEY `custid_idx` (`custid`),
+    CONSTRAINT `custid` FOREIGN KEY (`custid`) REFERENCES `ims`.`customers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE `ims`.`order_link`
+CREATE TABLE IF NOT EXISTS `ims`.`order_link`
 (
-    `idorder_link` INT NOT NULL AUTO_INCREMENT,
-    `orderid`      INT NOT NULL,
-    `itemid`       INT NOT NULL,
-    `quantity`     INT NOT NULL,
-    PRIMARY KEY (`idorder_link`)
+    `idorder_link` int NOT NULL AUTO_INCREMENT,
+    `orderid`      int NOT NULL,
+    `itemid`       int NOT NULL,
+    `quantity`     int NOT NULL,
+    PRIMARY KEY (`idorder_link`),
+    KEY `orderid_idx` (`orderid`),
+    KEY `itemid_idx` (`itemid`),
+    CONSTRAINT `itemid` FOREIGN KEY (`itemid`) REFERENCES `ims`.`item` (`iditem`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `ims`.`order` (`idorder`) ON DELETE CASCADE ON UPDATE CASCADE
 );
